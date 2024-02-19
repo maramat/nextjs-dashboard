42ccc3d7caf2dd05b286bca341bac22957bd4d92
maramat <maramat5@gmail.com>
2024-02-19 22:58:17 +0100

    Added some small changes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -24,7 +24,8 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
-
+    console.log('Data gathered:');
+    console.log(data);
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
